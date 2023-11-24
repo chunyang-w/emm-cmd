bdc8a99f52c75423068c62e13d5683dc6246a88e
chunyang.w <cw1722@ic.ac.uk>
2023-11-24 09:20:26 +0000

    comment out online test

--- test/test_run.py
+++ test/test_run.py
@@ -2,5 +2,5 @@ from emm.query.query import Query
 
 question = "What is the best way to install python packages?"
 
-res = Query().get_res(question)
-print(res)
+# res = Query().get_res(question)
+# print(res)
